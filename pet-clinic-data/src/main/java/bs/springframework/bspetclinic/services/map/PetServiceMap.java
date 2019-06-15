8397a5c0fd5a9f3463d48c1222a06afcfa281a2c
Ryan Robinson <rcrobinson89@gmail.com>
2019-06-14 22:30:48 -0700

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/bs/springframework/bspetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package bs.springframework.bspetclinic.services.map;
+
+import bs.springframework.bspetclinic.model.Pet;
+import bs.springframework.bspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
